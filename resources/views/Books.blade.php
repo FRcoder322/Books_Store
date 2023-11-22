b6faac40c9e5e9d10451b57b7d4ac1857e6e2a31
FRcoder322 <kuravalley@gmail.com>
2023-11-22 12:29:39 +0300

    changes

---
+++ resources/views/Books.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookStore</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <h1>Booking Form</h1>
+
+    <section>
+        <div style="margin-left:400px">
+            <form actions="">
+                <div>
+                <label for=""> Name</label>
+                <br>
+                <input type="text" name="name">
+                <br>
+                <label for=""> Pages</label>
+                <br>
+                <input type="text" name="Pages">
+                <br>
+                <label for=""> IBN</label>
+                <br>
+                <input type="text" name="IBN">
+                <br>
+                <label for=""> Category</label>
+                <br>
+                <input type="text" name="category">
+                <br>
+                <label for=""> Publisher</label>
+                <br>
+                <input type="text" name="publisher">
+                <br>
+                <label for=""> Year of Publication</label>
+                <br>
+                <input type="text" name="Y.O.P">
+                <br>
+                <label for="">Users</label>
+                <br>
+                <input type="text" name="User_id">
+                <br><br>
+                <button type="submit">Submit</button>
+                <br><br>
+                <button type="reset">Reset</button>
+                </div>
+
+        </form >
+        </div>
+    </section>
+</body>
+</html>
